2e0b3a1286a4edc5c9c576d334f73540eeb32fe6
procodesnet <www.procodes.net@gmail.com>
2022-12-28 13:23:49 +0100

    Add CSS, text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGit, faGithub, faJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import { faGit, faGithub, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div><FontAwesomeIcon icon={faReact} /> <FontAwesomeIcon icon={faJs} /></div>
-      <div><FontAwesomeIcon icon={faGithub} /> <FontAwesomeIcon icon={faGit} /></div>
+    <div>
+      <div>
+        Welcome to ProCodes
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faReact} style={{ color: "#61DAFB" }} id="Icon" />
+        <FontAwesomeIcon icon={faNodeJs} style={{ color: "#036E01" }} id="Icon" />
+      </div>
+      <div>
+        <FontAwesomeIcon icon={faGithub} id="Icon" />
+        <FontAwesomeIcon icon={faGit} id="Icon" />
+      </div>
+      <div>
+        B.V., certificated full stack developer
+      </div>
     </div>
   );
 }
