467eb17d0b304e944bc63d58f74f3ac0b0919546
procodesnet <www.procodes.net@gmail.com>
2022-12-30 18:26:01 +0100

    Add React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGit, faGithub, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons'
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <div>
-        Welcome to ProCodes
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faReact} style={{ color: "#61DAFB" }} id="Icon" />
-        <FontAwesomeIcon icon={faNodeJs} style={{ color: "#036E01" }} id="Icon" />
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faGithub} id="Icon" />
-        <FontAwesomeIcon icon={faGit} id="Icon" />
-      </div>
-      <div>
-        B.V., certificated full stack developer
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          Welcome to ProCodes
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <FontAwesomeIcon icon={faReact} id="Icon" style={{ color: "#61DAFB" }} />
+          <FontAwesomeIcon icon={faNodeJs} id="Icon" style={{ color: "#036E01" }} />
+          <FontAwesomeIcon icon={faGithub} id="Icon" />
+          <FontAwesomeIcon icon={faGit} id="Icon" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          B.V., full stack developer
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
